431147ec3f25aed77450d49885212212ef703056
Rishab Paruchuri <rishparu@gmail.com>
2021-07-04 16:40:41 +0530

    built player structure to transfer data

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Rishab Paruchuri on 04/07/21.
+//
+
+import Foundation
+
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
