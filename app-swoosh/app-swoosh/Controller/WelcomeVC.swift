d1299a677cbbf5f60a79fb4cfc600e3577a007f1
Rishab Paruchuri <rishparu@gmail.com>
2021-07-04 13:53:30 +0530

    Did some refactoring (Created View Controller Folder)

---
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Rishab Paruchuri on 02/07/21.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+}
+
